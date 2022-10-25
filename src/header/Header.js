13ad4136b44165d0f2b918c1b8b936e3bca6ba82
diego emiliano reartes <99770150+CodeReartes@users.noreply.github.com>
2022-10-24 22:12:08 -0300

    Add files via upload

---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+//hago un rfc por que tengo un complemento de fragmento de codificacion.
+import React from "react"
+ 
+export default function h1() {
+  return (
+    <div className="header-wrapper">
+      <h1 className="header-banner">Api Chuck Norris con React</h1>
+    </div>
+  )
+}
